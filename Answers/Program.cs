17def23ead7d3d950c74ab003f2af309aaec4f4c
Kenneth Fukizi <ken.fukizi@afrikancoder.com>
2023-03-20 17:22:08 +0200

    Hello to the C# Interviews world

---
+++ Answers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Answers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
